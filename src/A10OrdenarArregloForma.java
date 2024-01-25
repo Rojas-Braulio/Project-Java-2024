9c2ef2eed13c8a21e576099eb22f2e8a119b22ec
Rojas-Braulio <brau.rojas12@gmail.com>
2024-01-25 17:31:58 -0300

    Projects 25-01-2024

--- src/A10OrdenarArregloForma.java
+++ src/A10OrdenarArregloForma.java
@@ -3,16 +3,31 @@ import java.util.Scanner;
 public class A10OrdenarArregloForma {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int i, arreglo[];
-        arreglo = new int[10];
-        for (i = 0; i < 10; i++) {
-            System.out.println("Ingrese un numero: ");
-            arreglo[i] = scanner.nextInt();
+        //Creamos arreglo
+        int[] ar = new int[10];
+        int[] nuevoArreglo = new int[10];
+
+        //llenar
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = scanner.nextInt();
         }
-        System.out.println("El resultado es:");
-        for (i = 0; i <= 4; i++) {
-            System.out.println(arreglo[9 - i]);
-            System.out.println(arreglo[i]);
+        //ordenar
+        int contador = 0; // creamos contador para ir sumando sin afectar a los arrays
+        for (int i = 0; i < ar.length - i; i++) {
+
+            nuevoArreglo[contador++] = ar[ar.length - 1 - i]; // asignamos los arreglos anteriores al arreglo nuevo para no perder valores
+            nuevoArreglo[contador++] = ar[i];
+
+            // NuevoArreglo          ar
+            // D-0-0-0-0-0-0-0-0-0   1-2-3-4-5-6-7-8-9-D
+            // D-1-0-0-0-0-0-0-0-0   1-2-3-4-5-6-7-8-9-D
+            // D-1-9-0-0-0-0-0-0-0   1-2-3-4-5-6-7-8-9-D
+            // D-1-9-2-0-0-0-0-0-0   1-2-3-4-5-6-7-8-9-D
+            // ...
+        }
+
+        for (int a : nuevoArreglo) {
+            System.out.println(a);
         }
     }
 }
